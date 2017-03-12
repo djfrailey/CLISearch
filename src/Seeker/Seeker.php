117f72d0617dd9d83a0ddd540bf5cd4d87df79f7
David Frailey <david@snapshot.is>
2017-03-12 11:13:56 -0700

    Changed out curl calls for file_get_contents

--- src/Seeker/Seeker.php
+++ src/Seeker/Seeker.php
@@ -9,20 +9,7 @@ abstract class Seeker
     public function seek(string $term, int $page = 0) : string
     {
         $endpoint = $this->getSeekEndpoint($term, $page);
-
-        $options = [
-            CURLOPT_FOLLOWLOCATION => 1,
-            CURLOPT_MAXREDIRS => 2,
-            CURLOPT_RETURNTRANSFER => 1
-        ];
-        
-        $curl = curl_init($endpoint);
-        curl_setopt_array($curl, $options);
-        $response = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-
-        return $response;
+        return file_get_contents($endpoint);
     }
 
     abstract protected function getSeekEndpoint(string $term, int $page = 0) : string;
