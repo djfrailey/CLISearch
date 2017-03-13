f3b679a8f41bb9d00e3bfb8e779ed40c379bd20c
David Frailey <david@snapshot.is>
2017-03-13 13:00:28 -0700

    Removed State objects in favor for having the main application logic handle everything.

--- src/Application/CrawlApplication.php
+++ src/Application/CrawlApplication.php
@@ -9,65 +9,120 @@ use David\Console\Console;
 use David\Seeker\Seeker;
 use David\Bag\Bag;
 use David\State\CrawlApplicationState;
-use David\State\SearchState;
-use David\State\PaginationState;
+use \Generator;
 
 class CrawlApplication extends ConsoleApplication
 {
-   
+    const INPUT_STATE_SEARCH = 1;
+    const INPUT_STATE_PAGINATE = 2;
+    
+    private $inputState = self::INPUT_STATE_SEARCH;
     private $shouldRun = true;
-    private $applicationState;
+    private $searchParams;
     private $parser;
     private $seeker;
-    private $sharedApplicationData;
 
     public function __construct(Console $console, Seeker $seeker, ParserInterface $parser)
     {
         parent::__construct($console);
-        
         $this->seeker = $seeker;
         $this->parser = $parser;
-        $this->sharedApplicationData = new Bag();
-        
-        $defaultState = $this->createSearchState();
-        $this->setState($defaultState);
+        $this->searchParams = new Bag();
     }
 
     public function run()
     {
         while ($this->shouldRun) {
-            $this->applicationState->run();
-            $this->nextState();
+            
+            switch ($this->inputState) {
+                case self::INPUT_STATE_SEARCH:
+                    $this->handleSearchInputLoop();
+                break;
+                case self::INPUT_STATE_PAGINATE:
+                        $this->handlePaginateInputLoop();
+                break;
+            }
+
+            $searchTerm = $this->searchParams->get('searchTerm', '');
+            $page = $this->searchParams->get('page', 0);
+
+            if ($searchTerm) {
+                $seekResponse = $this->seeker->seek($searchTerm, $page);
+                $parsedData = $this->parser->parse($seekResponse);
+
+                $this->outputResults($parsedData);
+            }
         }
     }
 
-    public function setState(CrawlApplicationState $state)
+    private function handlePaginateInputLoop()
     {
-        $this->applicationState = $state;
-    }
+        $nextPage = $this->parser->getNextPage();
 
-    private function nextState()
-    {
-        $nextState = null;
-        
-        if ($this->applicationState instanceof SearchState) {
-            $nextState = $this->createPaginationState();
+        if ($nextPage > 0) {
+            $getNextPage = $this->getUserRequestsNextPage();
+
+            if ($getNextPage === false) {
+                $nextPage = 0;
+                $this->setInputState(self::INPUT_STATE_SEARCH);
+            }
         }
 
-        if ($this->applicationState instanceof PaginationState) {
-            $nextState = $this->createSearchState();
+        $this->searchParams->set('page', $nextPage);
+    }
+
+    private function handleSearchInputLoop()
+    {
+        $searchTerm = $this->getSearchTerm();
+        $this->searchParams->set('searchTerm', $searchTerm);
+        $this->setInputState(self::INPUT_STATE_PAGINATE);
+    }
+
+    private function getUserRequestsNextPage() : bool
+    {
+        $validInputReceived = false;
+        $userRequestsNextPage = false;
+
+        while ($validInputReceived === false) {
+            $input = $this->console->ask('Next Page [Y/N]?');
+            $input = strtolower($input);
+
+            if ($input === 'y' || $input === 'yes') {
+                $userRequestsNextPage = true;
+                $validInputReceived = true;
+            } elseif ($input === 'n' || $input === 'no') {
+                $validInputReceived = true;
+            }
         }
 
-        $this->setState($nextState);
+        return $userRequestsNextPage;
     }
 
-    private function createSearchState() : CrawlApplicationState
+    private function getSearchTerm() : string
     {
-        return new SearchState($this->console, $this->seeker, $this->parser, $this->sharedApplicationData);
+        $searchTerm = "";
+
+        while ($searchTerm == false) {
+            $searchTerm = $this->console->ask("Enter Query (SIGINT Exits)");
+        }
+
+        return $searchTerm;
     }
 
-    private function createPaginationState() : CrawlApplicationState
+    private function outputResults(Generator $results)
     {
-        return new PaginationState($this->console, $this->seeker, $this->parser, $this->sharedApplicationData);
+        $output = $this->console->getOutputStream();
+
+        foreach($results as $result) {
+            $output->writeLine('---');
+            $output->writeLine($result['title']);
+            $output->writeLine($result['href']);
+            $output->writeLine('---');
+        }
+    }
+
+    private function setInputState(int $state)
+    {
+        $this->inputState = $state;
     }
 }
